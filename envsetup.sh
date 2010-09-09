3177a2962fcad2052a5d7e4699d2dd834e749f45
Mehdi Abaakouk <theli48@gmail.com>
2010-09-09 09:55:47 +0200

    Add setup script

---
+++ envsetup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd $HOME
+
+typeset -a flist="zsh vimrc screenrc zshenv"
+
+haserror=
+error(){
+	echo "* file $f already exist (.$f = $(readlink -f .$f))"
+	haserror=1
+}
+
+for f in $flist; do 
+	[ -e ".$f" -a "$(readlink -f .$f)" != "$HOME/.env/$f" ] && error $f
+done
+
+[ -n "$haserror" ] && exit 1
+
+for f in $flist; do 
+	if [ ! -e ".$f" ] ; then
+		ln -sf .env/$f .$f
+	fi
+done
