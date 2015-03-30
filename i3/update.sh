7041ba642a28f353a6c1cf10ab917ed9c4f64af3
Mehdi Abaakouk <sileht@sileht.net>
2015-03-30 09:30:53 +0200

    Update i3bar script

--- i3/update.sh
+++ i3/update.sh
@@ -5,5 +5,23 @@ here=$(readlink -f $(dirname $0))
 if [ ! -d $here/venv ]; then
     virtualenv -p python3 $here/venv
 fi
-sudo apt-get install -y libiw-dev
-$here/venv/bin/pip install -U -r i3pystatus-reqs.txt
+dpkg -l libiw-dev >/dev/null 2>&1 || sudo apt-get install -y libiw-dev
+
+
+cat > i3pystatus-reqs.txt <<EOF
+-e git+https://github.com/enkore/i3pystatus#egg=i3pystatus
+netifaces
+basiciw
+colour<0.1.0
+pywapi
+keyring
+psutil
+EOF
+
+cleanup(){
+    rm -f i3pystatus-reqs.txt
+}
+trap 'cleanup' EXIT
+
+$here/venv/bin/pip install -U --allow-unverified pywapi --allow-external pywapi -r i3pystatus-reqs.txt
+
