1b4e19c6928d1515c8a23f41412d5c4740e129ce
Mehdi Abaakouk <sileht@sileht.net>
2015-09-15 15:15:14 +0200

    mutt: add missing files

---
+++ mutt/account.sileht.ml
@@ -0,0 +1,4 @@
+
+source ~/.mutt/account.sileht
+
+mailboxes =INBOX =Drafts =Junk =Sent =Trash =zml-april =zml-ceph =zml-check_mk =zml-debian =zml-debian-openstack =zml-fai-Locaux =zml-frnog =zml-os-all =zml-os-api =zml-os-ceilometer =zml-os-cinder =zml-os-devstack =zml-os-divers =zml-os-glance =zml-os-heat =zml-os-infra =zml-os-infradev =zml-os-ironic =zml-os-keystone =zml-os-neutron =zml-os-nova =zml-os-oslo =zml-os-puppet =zml-os-releases =zml-os-stable =zml-os-swift =zml-os-tc =zml-os-tripleo =zml-os-unknown =zml-os-users =zml-openwrt =zml-puppet-openstack =zml-tetalab =zml-toulibre =Queue
