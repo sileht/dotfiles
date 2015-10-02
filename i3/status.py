0fe5c19eed48e598e51759906709789eb4a64768
Mehdi Abaakouk <sileht@sileht.net>
2015-10-02 09:08:04 +0200

    bar tweaks

--- i3/status.py
+++ i3/status.py
@@ -3,15 +3,12 @@
 from i3pystatus import Status
 
 status = Status(standalone=True)
-status.register("pulseaudio", format="{volume_bar}♪",
-                multi_colors=True,
-                vertical_bar_width=1)
 status.register("clock", format="%a %b %d, %H:%m")
 status.register("weather", format="{current_temp}",
                 colorize=True,
                 location_code="FRXX0099:1:FR")
 status.register("battery",
-                format="{status}{remaining:%E%hh:%Mm} {bar} ",
+                format="{status}{remaining:%E%hh:%Mm}",
                 alert=True, alert_percentage=5,
                 status={"DIS": "↓", "CHR": "↑", "FULL": "="},
                 not_present_text="",
