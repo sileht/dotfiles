e6d67abaa8d35752462b23c3e30c20ecffc0ff69
Mehdi Abaakouk <sileht@sileht.net>
2015-10-05 11:07:05 +0200

    i3: add mail

--- i3/update.sh
+++ i3/update.sh
@@ -3,13 +3,13 @@
 here=$(readlink -f $(dirname $0))
 
 if [ ! -d $here/venv ]; then
-    virtualenv -p python3 $here/venv
+    virtualenv -p python3 --system-site-packages $here/venv
 fi
 dpkg -l libiw-dev >/dev/null 2>&1 || sudo apt-get install -y libiw-dev
 
 
 cat > i3pystatus-reqs.txt <<EOF
--e git+https://github.com/enkore/i3pystatus#egg=i3pystatus
+-e git+https://github.com/enkore/i3pystatus@master#egg=i3pystatus
 netifaces
 basiciw
 colour
@@ -25,6 +25,6 @@ cleanup(){
 }
 trap 'cleanup' EXIT
 
-ln -sf $here/livestatus.py $here/venv/src/i3pystatus/i3pystatus/
 $here/venv/bin/pip install -U --allow-unverified pywapi --allow-external pywapi -r i3pystatus-reqs.txt
+ln -sf $here/livestatus.py $here/venv/src/i3pystatus/i3pystatus/
 
