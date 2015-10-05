56ef57a6a195c808ccdcf1512247077a28bfc6eb
Mehdi Abaakouk <sileht@sileht.net>
2015-10-05 10:33:59 +0200

    i3: Change bar, add Mail

--- i3/status.py
+++ i3/status.py
@@ -1,6 +1,7 @@
 #!/home/sileht/.i3/venv/bin/python
 
 from i3pystatus import Status
+from i3pystatus.mail import imap
 
 status = Status(standalone=True)
 status.register("clock", format="%a %b %d, %H:%M")
@@ -16,6 +17,9 @@ status.register("battery",
 status.register("shell", command="/home/sileht/.i3/vpn-chk.sh",
                 hints={"markup": "pango"})
 
+status.register("mail",
+                hide_if_null=False,
+                backends=[imap.IMAP(host="mx1.sileht.net")])
 
 status.register("livestatus",
                 web_url="https://nagios.tetaneutral.net/check_mk/index.py?start_url=view.py%3Fselection%3D28fa9e16-bac7-40ad-b72c-18731527457d%26hst2%3D%26hst1%3D%26hst0%3Don%26is_service_acknowledged%3D0%26host_tag_2_op%3D%26st0%3D%26st1%3Don%26host_tag_2_grp%3D%26st3%3Don%26hdst2%3Don%26st2%3Don%26hstp%3Don%26neg_opthost_group%3D%26is_in_downtime%3D0%26host_tag_0_op%3D%26host_tag_2_val%3D%26host_tag_0_grp%3D%26host_tag_0_val%3D%26service_regex%3D%26host_tag_1_op%3D%26hdstp%3Don%26host_tag_1_grp%3D%26is_summary_host%3D0%26opthost_group%3Dall_without_tsf%26view_name%3Dsvcproblems%26stp%3D%26hdst3%3Don%26hdst0%3Don%26hdst1%3Don%26host_tag_1_val%3D%26is_service_in_notification_period%3D-1",  # noqa
