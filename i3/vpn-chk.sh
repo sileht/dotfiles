560fdeab6e1a98b1d1173cf93f78010dadf1be44
Mehdi Abaakouk <sileht@sileht.net>
2015-10-13 08:28:07 +0200

    i3: Fix vpn script

--- i3/vpn-chk.sh
+++ i3/vpn-chk.sh
@@ -2,8 +2,8 @@
 
 echo -n 'VPN: ' ; 
 {
-    pgrep -f '/var/run/openvpn/redhat.pid' >/dev/null 2>&1 && echo 'RedHat'
-    pgrep -f '/var/run/fastd.home.pid' >/dev/null 2>&1 && echo 'TTNN'
-    nmcli -m t -f TYPE,NAME c s --active | sed -n -e '/vpn[[:space:]]*/s///p' | sed 's/[[:space:]]*$//'
+    pgrep -F '/var/run/openvpn/redhat.pid' >/dev/null 2>&1 && echo 'RedHat'
+    pgrep -F '/var/run/fastd.home.pid' >/dev/null 2>&1 && echo 'TTNN'
+    [ -x "$(which nm-cli 2>/dev/null)" ] && nmcli -m t -f TYPE,NAME c s --active | sed -n -e '/vpn[[:space:]]*/s///p' | sed 's/[[:space:]]*$//'
 } | paste -sd ',' - | sed -e 's/^,//' -e 's/,$//' | sed -e 's/^$/n\/a/'
 exit 0
