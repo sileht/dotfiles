25833f7667ce6413ea95d5506f9657dcaf35468a
Mehdi Abaakouk <sileht@sileht.net>
2016-04-14 13:56:44 +0200

    Fix nm-cli command

--- i3/vpn-chk.sh
+++ i3/vpn-chk.sh
@@ -4,6 +4,6 @@ echo -n 'VPN: ' ;
 {
     pgrep -F '/var/run/openvpn/redhat.pid' >/dev/null 2>&1 && echo 'RedHat'
     pgrep -F '/var/run/fastd.home.pid' >/dev/null 2>&1 && echo 'TTNN'
-    [ -x "$(which nm-cli 2>/dev/null)" ] && nmcli -m t -f TYPE,NAME c s --active | sed -n -e '/vpn[[:space:]]*/s///p' | sed 's/[[:space:]]*$//'
+    [ -x "$(which nmcli 2>/dev/null)" ] && nmcli -m t -f TYPE,NAME c s --active | sed -n -e '/vpn[[:space:]]*/s///p' | sed 's/[[:space:]]*$//'
 } | paste -sd ',' - | sed -e 's/^,//' -e 's/,$//' | sed -e 's/^$/n\/a/'
 exit 0
