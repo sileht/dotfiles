d708c35ece2add0dd2e8669ee4191a3da7d54454
Mehdi Abaakouk <sileht@sileht.net>
2017-02-22 08:56:45 +0100

    Update mutt setup

--- mutt/mail_html.sh
+++ mutt/mail_html.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 charset=$1
-shift
-
-links -dump -html-numbered-links 1 -html-images 1 -html-assume-codepage $charset "$@"
-if [ $? -ne 0 ]; then
-    links -dump -html-numbered-links 1 -html-images 1  "$@"
+#file=$2
+#shift
+#elinks -dump 1 -dump-color-mode 3 -dump-charset $charset "$file"
+#if [ $? -ne 0 ]; then
+    links -dump -html-numbered-links 1 -html-images 1 -html-assume-codepage $charset "$@"
     if [ $? -ne 0 ]; then
-        cat "$@"
+        links -dump -html-numbered-links 1 -html-images 1  "$@"
+        if [ $? -ne 0 ]; then
+            cat "$@"
+        fi
     fi
-fi
+#fi
