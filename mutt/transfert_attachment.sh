ecfa01054922d53ed6b5ca234ed5d15f1af2b353
Mehdi Abaakouk <sileht@sileht.net>
2017-04-06 09:03:26 +0200

    mutt: Fix mailcap

--- mutt/transfert_attachment.sh
+++ mutt/transfert_attachment.sh
@@ -3,20 +3,12 @@
 # Expect ssh session with reverse forwarding like -R62222:localhost:2222
 # and ssh-agent and ssh keys setuped
 
-FILENAME=$1
-MIMETYPE=$2
+FILENAME="$1"
 REMOTE_DIR="/tmp"
 BASENAME=$(basename "${FILENAME}")
 REMOTE_FILE="${REMOTE_DIR}/${BASENAME}"
 
-scp -P 62222 $FILENAME sileht@localhost:$REMOTE_FILE
-ssh -p 62222 sileht@localhost "chmod 600 '${REMOTE_FILE}' ; DISPLAY=:0 xdg-open '$REMOTE_FILE'"
-
-#COMMAND="chmod 600 '${REMOTE_FILE}' ; DISPLAY=:0 run-mailcap --action=view '"
-#[ -n "${MIMETYPE}" -a "${MIMETYPE}" != "application/octet-stream" ] && COMMAND="${COMMAND}${MIMETYPE}:"
-#COMMAND="${COMMAND}${REMOTE_FILE}' > /dev/null & sleep 2"
-# ssh -p 62222 sileht@localhost "${COMMAND}"
-
+cat "$FILENAME" | ssh -p 62222 sileht@localhost "cat > '$REMOTE_FILE' ; chmod 600 '${REMOTE_FILE}' ; DISPLAY=:0 run-mailcap --action=view '$REMOTE_FILE'"
 
 clear
 exit 0
