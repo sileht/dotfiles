fcab7a872ee270748a88138e1d20ee0839e245d1
Mehdi Abaakouk <sileht@sileht.net>
2017-04-25 08:00:25 +0200

    mutt debug

--- mutt/transfert_attachment.sh
+++ mutt/transfert_attachment.sh
@@ -9,6 +9,9 @@ BASENAME=$(basename "${FILENAME}")
 REMOTE_FILE="${REMOTE_DIR}/${BASENAME}"
 
 cat "$FILENAME" | ssh -p 62222 sileht@localhost "cat > '$REMOTE_FILE' ; chmod 600 '${REMOTE_FILE}' ; DISPLAY=:0 run-mailcap --action=view '$REMOTE_FILE'"
-
+if [ $? -ne 0 ]; then
+    echo "Error during $(basename $0) execution."
+    read
+fi
 clear
 exit 0
