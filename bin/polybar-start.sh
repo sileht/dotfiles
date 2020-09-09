1af8f27b5b3980f056aa1a261cf76026c791ed4b
Mehdi Abaakouk <sileht@sileht.net>
2020-09-09 23:56:16 +0200

    fixes

--- bin/polybar-start.sh
+++ bin/polybar-start.sh
@@ -7,7 +7,7 @@ TRAY_POSITION=center polybar -c ~/.env/polybar/config.ini top &
 DISPLAY_NB=$(xrandr | grep ' connected' | wc -l)
 
 if [ "$DISPLAY_NB" -ge 2 ]; then
-    DISPLAY_AUX=$(xrandr | grep ' connected' | grep -v primary | awk '{print $1}')
+    DISPLAY_AUX=$(xrandr | grep ' connected' | grep -v primary | tail -1 | awk '{print $1}')
     [ ! "$DISPLAY_AUX" ] && DISPLAY_AUX=$(xrandr | grep ' connected' | tail -1 | awk '{print $1}')
     DISPLAY_AUX=${DISPLAY_AUX} polybar -c ~/.env/polybar/config.ini aux &
 fi
