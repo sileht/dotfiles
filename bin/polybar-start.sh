b40f7e1dd85c678ab477559e9a5bfffaf0f789e8
Mehdi Abaakouk <sileht@sileht.net>
2020-11-11 00:29:43 +0100

    multiple screen polybar fix

--- bin/polybar-start.sh
+++ bin/polybar-start.sh
@@ -6,10 +6,10 @@ DISPLAYS=$(xrandr | awk '/ connected/{print $1}')
 DISPLAY_PRIMARY=$(xrandr | awk '/ connected primary/{print $1}')
 [ ! "$DISPLAY_PRIMARY" ]  && DISPLAY_PRIMARY=$(xrandr | awk '/ connected/{print $1;exit;}')
 
-echo TRAY_POSITION=center polybar -c ~/.env/polybar/config.ini top &
+TRAY_POSITION=center polybar -c ~/.env/polybar/config.ini top &
 for display in $DISPLAYS; do
     [ "$display" == "$DISPLAY_PRIMARY" ] && continue
-    echo DISPLAY_AUX=${display} polybar -c ~/.env/polybar/config.ini aux &
+    DISPLAY_AUX=${display} polybar -c ~/.env/polybar/config.ini aux &
 done
 
 wait
