df4403a08ae301f422454399ed0cffe479d18ed8
Mehdi Abaakouk <sileht@sileht.net>
2021-06-18 20:18:13 +0200

    timing restart

--- bin/polybar-start.sh
+++ bin/polybar-start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-pkill -x polybar
-
 DISPLAYS=$(xrandr | awk '/ connected/{print $1}')
 DISPLAY_PRIMARY=$(xrandr | awk '/ connected primary/{print $1}')
 [ ! "$DISPLAY_PRIMARY" ]  && DISPLAY_PRIMARY=$(xrandr | awk '/ connected/{print $1;exit;}')
 
+pkill -x polybar
+sleep 0.4
 TRAY_POSITION=center polybar -c ~/.env/polybar/config.ini top &
 for display in $DISPLAYS; do
     [ "$display" == "$DISPLAY_PRIMARY" ] && continue
     DISPLAY_AUX=${display} polybar -c ~/.env/polybar/config.ini aux &
 done
-
-wait
